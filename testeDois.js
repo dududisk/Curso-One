ddb25f9c9aa0e5b5e39a9dc15c7e9c234730489e
Dudu Matias <engedumatias@gmail.com>
2025-01-16 12:44:22 -0300

    teste dois com exeplos dos professores

--- testeDois.js
+++ testeDois.js
@@ -32,3 +32,38 @@ alert(`O saldo da sua conta é: R$ ${saldo.toFixed(2)}`);
 // 5. Boas-vindas ao usuário pelo nome
 const nomeUsuario = prompt("Digite seu nome:");
 alert(`Bem-vindo(a), ${nomeUsuario}!`);
+
+// OUTROS METODOS DE RESOLUÇÃO DOS PROFESSORES
+// 1
+// diaDaSemana = prompt('Qual é o dia da semana?');
+// if (diaDaSemana == 'Sábado') {
+//     alert('Bom fim de semana!');
+// } else if (diaDaSemana == 'Domingo') {
+//     alert('Bom fim de semana!');
+// } else {
+//     alert('Boa semana!');
+// }
+
+// 2
+// numero = prompt('Digite um positivo ou negativo');
+// if (numero > 0) {
+//     alert('Número positivo!');
+// } else {
+//     alert('Número negativo!');
+// }
+
+// 3
+// pontuacao = 105;
+// if (pontuacao >= 100) {
+//     console.log('Parabéns, você venceu!');
+// } else {
+//     console.log('Tente novamente para ganhar.');
+// }
+
+// 4
+// let saldoConta = 500; 
+// alert(`Seu saldo é de R$${saldoConta}.`);
+
+// 5
+// let nome = prompt('Qual o seu nome?');
+// alert(`Boas vindas ${nome}`);
